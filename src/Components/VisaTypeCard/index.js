62d9cd68c080e90e1b93c59cbca478e7fb4151a6
vedavyas1993 <76591323+vedavyas1993@users.noreply.github.com>
2023-08-31 15:15:19 +0530

    added cards to visa page

--- src/Components/VisaTypeCard/index.js
+++ src/Components/VisaTypeCard/index.js
@@ -22,11 +22,48 @@ function VisaCardType({ type, citizenship }) {
     <div
       style={{
         width: "100%",
+        height: "100%",
         display: "flex",
         flexDirection: "column",
         gap: "15px",
+        border: "1px solid #ddd",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        paddingBottom: "20px",
+        borderRadius: "20px",
+        // overflow: "hidden",
       }}
     >
+      <div
+        className="d-flex justify-content-center align-items-center text-capitalize"
+        style={{
+          backgroundImage: "url(/assets/passport/passport5.png)",
+          backgroundColor: "transparent",
+          backgroundPosition: "right bottom",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "scroll",
+          minHeight: "200px",
+          borderRadius: "20px",
+          padding: "20px",
+        }}
+      >
+        {/* <img
+          src="/assets/passport/passport5.png"
+          style={{ margin: "auto", width: "100%" }}
+        ></img> */}
+        <h2
+          className="text-center text-light"
+          style={{
+            fontSize: "22px",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            textDecoration: "underline",
+          }}
+        >
+          {type + " Visa for " + citizenship + " Citizens"}
+        </h2>
+      </div>
+
       <div
         className="search1"
         style={{
