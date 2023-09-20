f7c54b8a5183e996dd3660722023de7bb95ea9f7
G T Haika <69863568+hari-65@users.noreply.github.com>
2023-09-20 14:56:29 +0530

    Added some changes in Home page and add search bar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,22 @@
 import styles from "./Home.module.css";
-import Search from "../../Components/SearchCountry/Search";
 import { Link } from "react-router-dom";
 import { GiOpenBook } from "react-icons/gi";
 import { BsCheck2Circle } from "react-icons/bs";
 import { FaAddressBook, FaUserGraduate } from "react-icons/fa";
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 import Slider from "react-slick";
 import React, { useState } from 'react';
 import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+import ReactFlagsSelect from "react-flags-select";
+
+
 
 function Home() {
+
+  const [select, setSelect] = useState("SE");
+  const onSelect = (code) => setSelect(code);
+  console.log("SELECT", select);
+
   var settings = {
     dots: true,
     infinite: false,
@@ -46,7 +52,7 @@ function Home() {
     ],
   };
 
-
+ 
 
   return (
     <>
@@ -60,8 +66,51 @@ function Home() {
                 Experience Seamless World Travel with Our<br></br> Visa and
                 Passport Specialists
               </h1>
+          
+            <div className={`${styles["Home-Searchbar"]}`}>
+            <button className={`${styles["e-visa-button"]}`}>Apply for E-Visa Travel</button>
+            <div className={`${styles["Home-country-container"]}`}>
+               <div className={`${styles["Home-country"]}`}>
+               <ReactFlagsSelect
+               selected={select}
+               onSelect={onSelect}
+               countries={["AF","AL","DZ","AS","AD","AO","AI","AQ",
+               "AG","AR","AM","AW","AU","AT","AZ",
+               "BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BA","BW","BR","IO","BN","BG","BF","BI",
+               "CV","KH","CM","CA","KH","CM","CA","KY","CF","TD","CL","CN","CX","CC","CO","KM","CD","CG","CK","CR","HR","CU","CW","CY","CZ","CI","DK","DJ","DM","DO","EC",
+               "EG","SV","GQ","ER","EE","SZ","ET","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP",
+               "GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ",
+               "KE","KI","KP","KR","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MG","MW","MY","MV","ML","MT","MH","MQ","MR",
+               "MU","YT","MX","FM","MD","MC","MN","ME","MA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MP","NO","OM","PK","PW","PS",
+               "PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","CG","RE","RO","RU","RW","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST",
+               "SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","KR","SS","ES","LK","SD","SR","SJ","SZ","SE","CH","SY","TW","TJ",
+               "TZ","TH","TG","TK","TO","TT","TN","TR","TM","TC","TV","VI","UG","UA","AE","GB","US","UY","UZ","VU","VA","VE","VN","WF","EH",
+               "YE","ZM","ZW"]}
+             />
+               </div>
+               <div className={`${styles["Home-country"]}`}>
+               <ReactFlagsSelect
+               selected={select}
+               onSelect={onSelect}
+               countries={["AF","AL","DZ","AS","AD","AO","AI","AQ",
+               "AG","AR","AM","AW","AU","AT","AZ",
+               "BS","BH","BD","BB","BY","BE","BZ","BJ","BM","BT","BO","BA","BW","BR","IO","BN","BG","BF","BI",
+               "CV","KH","CM","CA","KH","CM","CA","KY","CF","TD","CL","CN","CX","CC","CO","KM","CD","CG","CK","CR","HR","CU","CW","CY","CZ","CI","DK","DJ","DM","DO","EC",
+               "EG","SV","GQ","ER","EE","SZ","ET","FK","FO","FJ","FI","FR","GF","PF","TF","GA","GM","GE","DE","GH","GI","GR","GL","GD","GP",
+               "GU","GT","GG","GN","GW","GY","HT","HM","VA","HN","HU","IS","IN","ID","IR","IQ","IE","IM","IL","IT","JM","JP","JE","JO","KZ",
+               "KE","KI","KP","KR","KW","KG","LA","LV","LB","LS","LR","LY","LI","LT","LU","MO","MG","MW","MY","MV","ML","MT","MH","MQ","MR",
+               "MU","YT","MX","FM","MD","MC","MN","ME","MA","NR","NP","NL","NC","NZ","NI","NE","NG","NU","NF","MP","NO","OM","PK","PW","PS",
+               "PA","PG","PY","PE","PH","PN","PL","PT","PR","QA","CG","RE","RO","RU","RW","BL","SH","KN","LC","MF","PM","VC","WS","SM","ST",
+               "SA","SN","RS","SC","SL","SG","SX","SK","SI","SB","SO","ZA","KR","SS","ES","LK","SD","SR","SJ","SZ","SE","CH","SY","TW","TJ",
+               "TZ","TH","TG","TK","TO","TT","TN","TR","TM","TC","TV","VI","UG","UA","AE","GB","US","UY","UZ","VU","VA","VE","VN","WF","EH",
+               "YE","ZM","ZW"]}
+               />
+               </div>
+               <button className={`${styles["Home-country-go-button"]}`}>Go</button>
+            </div>
+            </div>
             </center>
-          </div>
+           </div>
          
         </div>
         {/* <div id="carousel-container">
